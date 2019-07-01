1c6132b9ace6dab96801f937b9b0c9a6e065965a
mrdrivingduck <562655624@qq.com>
2019-07-01 19:37:09 +0800

    Add submenu

--- src/components/DucklingAside.vue
+++ src/components/DucklingAside.vue
@@ -15,16 +15,27 @@
 
       <el-submenu>
         <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>导航一</span>
+          <i class="el-icon-edit"></i>
+          <span> Notes </span>
         </template>
+
+        <el-submenu>
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>Java</span>
+          </template>
+
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+
+        </el-submenu>
 
       </el-submenu>
 
       <el-submenu>
         <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>导航二</span>
+          <i class="el-icon-document"></i>
+          <span> Paper Outline </span>
         </template>
 
       </el-submenu>
