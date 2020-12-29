96bd95c7103cdc54e726285b9d178cb10d22a9c6
mrdrivingduck <562655624@qq.com>
2020-12-29 14:37:03 +0800

    Theme change for el-backtop

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!-- 
 
   @author - Mr Dk.
-  @version - 2020/03/02
+  @version - 2020/12/29
 
   @description - 
     The entry component.
@@ -12,7 +12,7 @@
   <div id="app" ref="rootDiv">
 
     <!-- Back to top -->
-    <el-backtop></el-backtop>
+    <el-backtop v-bind:style="{ backgroundColor: this.backTopColor }"/>
 
     <!-- Header -->
     <el-header>
@@ -60,7 +60,8 @@ export default {
   },
   data() {
     return {
-      theme: null
+      theme: null,
+      backTopColor: null,
     };
   },
   methods: {
@@ -70,6 +71,7 @@ export default {
       const allThemes = this.$store.state.theme.themes;
       const themeIndex = this.$store.state.theme.currentThemeIndex;
       this.theme = allThemes[themeIndex].name.toLowerCase();
+      this.backTopColor = allThemes[themeIndex].card.backgroundColor;
     },
 
     // Set the background color of HTML page
